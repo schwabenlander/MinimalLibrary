fb3b4aef83e731cbaee47ab9519d918a511d0ef1
Sean Schwabenlander <sean@schwabendesign.com>
2022-04-26 12:24:38 -0500

    Added integrations tests for getting a book

--- MinimalLibrary.Api.Tests.Integration/LibraryEndpointsTests.cs
+++ MinimalLibrary.Api.Tests.Integration/LibraryEndpointsTests.cs
@@ -79,6 +79,38 @@ public class LibraryEndpointsTests
         error.PropertyName.Should().Be("Isbn");
         error.ErrorMessage.Should().Be("A book with this ISBN already exists");
     }
+
+    [Fact]
+    public async Task GetBook_ReturnsBook_WhenBookExists()
+    {
+        // Arrange
+        var httpClient = _factory.CreateClient();
+        var book = GenerateBook();
+        await httpClient.PostAsJsonAsync("/books", book);
+        _createdIsbns.Add(book.Isbn);
+        
+        // Act
+        var result = await httpClient.GetAsync($"books/{book.Isbn}");
+        var existingBook = await result.Content.ReadFromJsonAsync<Book>();
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        existingBook.Should().BeEquivalentTo(book);
+    }
+    
+    [Fact]
+    public async Task GetBook_ReturnsNotFound_WhenBookDoesNotExist()
+    {
+        // Arrange
+        var httpClient = _factory.CreateClient();
+        var isbn = GenerateIsbn();
+        
+        // Act
+        var result = await httpClient.GetAsync($"books/{isbn}");
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
     
     private Book GenerateBook(string title = "The Dirty Coder")
     {
